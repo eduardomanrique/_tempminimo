26fd5de2d194e65432419d93e812bb410c35611f
Eduardo Kitamura Manrique <eduardo.manrique@mercadolivre.com>
2017-09-21 10:32:47 -0300

    added test

--- test/htmlParser.js
+++ test/htmlParser.js
@@ -16,6 +16,7 @@ describe('Test html parser', function() {
     expect(textWitScript).is.equal('Test 123 <xscr scr="fn(1,2,&quot;3&quot;) + &quot;x&quot;"></xscr>, lorem');
     //_htmlParser._isEmptyText
     expect(_htmlParser.__get__('_eqIgnoreCase')('asdf', 'ASdf')).is.equal(true);
+	expect(_htmlParser.__get__('_eqIgnoreCase')('asdf', 'ssdf')).is.equal(false);
     expect(_htmlParser.__get__('_validateJS')('var a = 1;')).is.equal(true);
     expect(_htmlParser.__get__('_validateJS')('var a s s')).is.equal(false);
   });
